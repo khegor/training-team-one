25c00c342f82e121413cbccb991ca99568ae86c3
khegor <egor1234@gmail.com>
2017-10-23 16:06:14 +0300

    adding random

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,14 +1,11 @@
 import com.roxoft.trainingteamone.converter.ConverterUtil;
-import com.roxoft.trainingteamone.dao.AutobusDao;
-import com.roxoft.trainingteamone.dao.CarDao;
-import com.roxoft.trainingteamone.dao.RoadDao;
-import com.roxoft.trainingteamone.daoImpl.CarDaoImpl;
-import com.roxoft.trainingteamone.daoImpl.RoadDaoImpl;
 import com.roxoft.trainingteamone.logic.Algorithm;
 import com.roxoft.trainingteamone.models.*;
 import com.roxoft.trainingteamone.services.*;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -16,33 +13,50 @@ import java.util.Random;
  * Created by Esenin on 02.09.2017.
  */
 public class MainClass {
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		BasicConfigurator.configure();
 
-		RoadService roadService = new RoadService();
-		List<Road> roads = roadService.getAllRoads();
-		Algorithm algorithm = new Algorithm(roads);
-		Crossroad fromCrossroad = new Crossroad();
-		fromCrossroad.setId(1);
-		Crossroad toCrossroad = new Crossroad();
-		toCrossroad.setId(6);
-		System.out.println(algorithm.getMaxFlow(fromCrossroad, toCrossroad));
+        BasicConfigurator.configure();
+        final Logger LOGGER = Logger.getLogger(MainClass.class);
 
-		CarDao carDAO = new CarDaoImpl();
+        CarService carService = new CarService();
+        RoadService roadService = new RoadService();
+        List<Road> roads = roadService.getAllRoads();
+        Algorithm algorithm = new Algorithm(roads);
 
-		List<Car> cars = carDAO.getAllCars();
+        Random random = new Random();
 
-		ConverterUtil.toJSON(cars, "src\\cars.json");
-		
-		RoadDao roadDAO = new RoadDaoImpl();
-		Road road = new Road();
-		road.setFromCrossroad(new Crossroad());
-		road.setToCrossroad(new Crossroad());
-		Random r = new Random();
-		road.setName("road1");
-		double capacity = r.nextDouble();
-		road.setDailyCarryingCapacity(capacity);
-		roadDAO.createRoad(road);
-	}
+        for (Road road : roads) {
+
+            int capacity = random.nextInt(100);
+            road.setDailyCarryingCapacity(capacity);
+            roadService.updateRoad(road);
+        }
+
+        roadService.getAllRoads();
+        Crossroad fromCrossroad = new Crossroad();
+        fromCrossroad.setId(1);
+        Crossroad toCrossroad = new Crossroad();
+        toCrossroad.setId(6);
+        double maxFlow = algorithm.getMaxFlow(fromCrossroad, toCrossroad);
+
+        List<Car> randomCars = new ArrayList<>();
+        for (int i = 0; i < random.nextInt(20); i++) {
+            randomCars.add(carService.getCarById(random.nextInt(20)));
+        }
+
+        LOGGER.info("\n MAX ROAD CAPACITY FOR EXISTING PATH " + maxFlow);
+        double sumWeight = 0;
+        List<Car> resultCars = new ArrayList<>();
+        for (Car car : randomCars) {
+            sumWeight = sumWeight + car.getMaxWeight();
+            if (sumWeight > maxFlow) {
+                LOGGER.error("ROADS ARE FULLY EMPTY OR HAVEN'T NEEDED CARRYING CAPACITY FOR EXISTING WAY OR TRANSPORT");
+                break;
+            }
+            LOGGER.info("\n TRANSPORT " + car.getModel() + " CAN RIDE WITH WEIGHT " + car.getMaxWeight()+ "\n");
+            resultCars.add(car);
+        }
+        ConverterUtil.toJSON(resultCars, "src\\main\\resources\\cars.json");
+    }
 }
