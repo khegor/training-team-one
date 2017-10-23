c16f4799ddfcd3b4a08dc58be7764c2317a398b2
khegor <egor1234@gmail.com>
2017-10-23 16:07:03 +0300

    converterUtil replacment to converter pack

--- src/main/java/com/roxoft/trainingteamone/converter/ConverterUtil.java
+++ src/main/java/com/roxoft/trainingteamone/converter/ConverterUtil.java
@@ -1,18 +1,21 @@
 package com.roxoft.trainingteamone.converter;
 
 import java.io.File;
-
+import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class ConverterUtil {
 
+	public static final Logger LOGGER = Logger.getLogger(ConverterUtil.class);
+
 	public static void toJSON(Object o, String path) {
+
 		ObjectMapper mapper = new ObjectMapper();
 
 		try {
 			mapper.writeValue(new File(path), o);
 		} catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.error(e);
 		}
 	}
 }
